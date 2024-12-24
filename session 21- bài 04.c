09bf74db1b579b25754150ae00471c15abf0cdc6
quangduymanh <quangduymanh4@gmail.com>
2024-12-25 00:04:43 +0700

    upload bài 04

---
+++ session 21- bài 04.c
@@ -0,0 +1,45 @@
+#include<stdio.h>
+int main(){
+	char string[100]={0};
+	FILE *f;
+	f = fopen("D:/Dev c rikki/bt01.txt","w");	
+	if (f == NULL) {
+        printf("Khong mo duoc tep.\n");
+        return 1;
+    }
+    printf("nhap chuoi ky tu: ");
+    fgets(string,sizeof(string),stdin);
+    fputs(string,f);
+    fclose(f);
+    printf("da ghi xong chuoi ky tu vao tep thanh cong\n");
+    f = fopen("D:/Dev c rikki/bt01.txt","r");
+    if(f==NULL){
+    	printf("khong mo duoc tep\n");
+    	return 1;
+	}
+    char first = fgetc(f);
+    if (first == EOF){
+    	printf("tep rong \n");
+	}else{
+		printf("ky tu dau tien trong tep: %c\n",first);
+	}
+	f = fopen("D:/Dev c rikki/bt01.txt", "a");
+    printf("Nhap chuoi ky tu de ghi them vao tep: ");
+    fgets(string, sizeof(string), stdin);
+    size_t len = strlen(string);
+    if (len > 0 && string[len - 1] == '\n') {
+        string[len - 1] = '\0';
+    }
+    fputs(string, f);
+    fputs("\n", f);
+    fclose(f);
+    printf("Da ghi xong chuoi ky tu vao tep thanh cong.\n");
+    f = fopen("D:/Dev c rikki/bt01.txt", "r");
+    if (fgets(string, sizeof(string), f) != NULL) {
+        printf("Dong dau tien trong tep: %s\n", string);
+    } else {
+        printf("Tep rong\n");
+    }
+    fclose(f);
+    return 0;
+}
